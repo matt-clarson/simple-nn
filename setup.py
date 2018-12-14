9277abb5f7024140af82a979eb26810a3506ef93
matt-clarson <clarson469@googlemail.com>
2018-12-14 08:42:44 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description=f.read()
+
+setup(
+    name='simple-nn',
+    version='0.1.0',
+    description='A Simple, Handwritten Neural Network',
+    long_description=long_description,
+    license='MIT',
+    author='matt clarson',
+    author_email='clarson469@googlemail.com',
+    packages=['simplenn'],
+    install_requires=['numpy']
+)
