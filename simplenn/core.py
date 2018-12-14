4cc731567f6f7fc15d3c8935fd14174f88551019
matt-clarson <clarson469@googlemail.com>
2018-12-14 19:32:19 +0000

    redesigned to allow for variable number of layers

--- simplenn/core.py
+++ simplenn/core.py
@@ -4,42 +4,68 @@ import functools
 import simplenn.softmax as softmax
 import simplenn.helpers as helpers
 
+
+def hidden_layer(input_layer, weights):
+    output = np.maximum(0, input_layer.dot(weights))
+    def backprop(prev_diff):
+        prev_diff[output <= 0] = 0
+        d_input = prev_diff.dot(weights.T)
+        d_weights = input_layer.T.dot(prev_diff)
+        return d_input, d_weights
+    return output, backprop
+
+def output_layer(input_layer, weights, expected_output, regularised_loss):
+    output = input_layer.dot(weights)
+    data_loss, probs = softmax.softmax_loss(output, expected_output)
+    loss = data_loss + regularised_loss
+    def backprop():
+        diff = softmax.softmax_diff(output, expected_output, probs)
+        d_input = diff.dot(weights.T)
+        d_weights = input_layer.T.dot(diff)
+        return d_input, d_weights
+    return loss, output, backprop
+
+
 def train(X, y, num_classes, **params):
-    hidden_size = params.setdefault('hidden_size', 100)
+    X = np.hstack((X, np.ones((X.shape[0], 1))))
+
     iterations = params.setdefault('iterations', 10000)
-    step_size = params.setdefault('step_size', 1.0)
+    step_size = params.setdefault('step_size', 0.01)
     reg_strength = params.setdefault('reg_strength', 1e-3)
+    network_shape = [X.shape[1]] + params.setdefault('network_shape', [100]) + [num_classes]
 
-    W1, b1 = helpers.init_weights(X.shape[1], hidden_size)
-    W2, b2 = helpers.init_weights(hidden_size, num_classes)
-
+    initialise_weights = lambda m, n: np.random.randn(m, n) * 0.001
+    weights = [initialise_weights(network_shape[i-1], n) for i, n in enumerate(network_shape) if i != 0]
+    print(f'weights: {[W.shape for W in weights]}')
     for i in range(iterations):
-        hidden_layer, output_layer = helpers.evaluate(
-            X,
-            W1, b1, W2, b2,
-            cache=True
-        )
+        accumulator = lambda acc, x: acc + 0.5*reg_strength*np.sum(x*x)
+        regularised_loss = functools.reduce(accumulator, weights, 0)
+        layers = []
+        for j, W in enumerate(weights[:-1]):
+            layers.append(hidden_layer(X if j == 0 else layers[-1][0], W))
     
-        loss, probs = softmax.softmax_loss(
-            output_layer, y, reg_strength,
-            [W1, W2]
-        )
-
-        if i % 1000 == 0 :
-            print(f'loss at iteration {i}: {loss}')
+        loss, output, output_backprop = output_layer(layers[-1][0], weights[-1], y, regularised_loss)
+        if i % 1000 == 0:
+            print(f' loss at iteration {i}: {loss}')
+        layers.append((output, output_backprop))
     
-        d_W1, d_b1, d_W2, d_b2 = helpers.backpropagate(
-            output_layer, hidden_layer, X, y, probs,
-            W1, b1, W2, b2,
-            reg_strength
-        )
-
-        W1, b1, W2, b2 = helpers.update_weights(
-            [W1, b1, W2, b2], [d_W1, d_b1, d_W2, d_b2],
-            step_size
-        )
+        d_weights = []
+        prev_diff = None
+        for k, layer in enumerate(reversed(layers)):
+            backprop = layer[1]
+            d_input, d_W = backprop() if k == 0 else backprop(prev_diff)
+            prev_diff = d_input
+            d_weights.append(d_W)
     
-    return W1, b1, W2, b2
+        weights = [weights[l] - step_size*d_W for l, d_W in enumerate(reversed(d_weights))]
+    
+    # end for
+    return weights
 
 def classify(X, *weights):
-    return np.argmax(helpers.evaluate(X, *weights), axis=1)
+    prev_layer = np.hstack((X, np.ones((X.shape[0], 1))))
+    for W in weights[:-1]:
+        prev_layer = np.maximum(0, prev_layer.dot(W))
+    output = prev_layer.dot(weights[-1])
+    return np.argmax(output, axis=1)
+
--- simplenn/softmax.py
+++ simplenn/softmax.py
@@ -1,14 +1,12 @@
 import numpy as np
 import functools
 
-def softmax_loss(scores, target, reg_strength, weights):
+def softmax_loss(scores, target):
     num_examples = scores.shape[0]
     exp_scores = np.exp(scores)
     probabilities = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
     correct_logprobs = -np.log(probabilities[range(num_examples), target])
-    data_loss = np.sum(correct_logprobs) / num_examples
-    accumulate_reg_loss = lambda acc, W: acc + (0.5*reg_strength*np.sum(W*W))
-    loss = functools.reduce(accumulate_reg_loss, weights, data_loss)
+    loss = np.sum(correct_logprobs) / num_examples
     return loss, probabilities
 
 def softmax_diff(scores, target, probabilities):
