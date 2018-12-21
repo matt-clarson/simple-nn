1bd32e6db2826ee91ce45eebe26c60cfa4586d48
matt-clarson <clarson469@googlemail.com>
2018-12-21 21:25:05 +0000

    modified softmax to imporve numerical stability

--- simplenn/softmax.py
+++ simplenn/softmax.py
@@ -1,8 +1,11 @@
 import numpy as np
 import functools
 
+stabilise_softmax_input = lambda f: f - np.max(f)
+
 def softmax_loss(scores, target):
     num_examples = scores.shape[0]
+    scores = stabilise_softmax_input(scores)
     exp_scores = np.exp(scores)
     probabilities = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
     correct_logprobs = -np.log(probabilities[range(num_examples), target])
