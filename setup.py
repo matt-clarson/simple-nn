21fb5b7594076a0c95e301aebc857882070a3a4c
matt-clarson <clarson469@googlemail.com>
2018-12-23 12:45:40 +0000

    incrementing version as MINOR

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='simple-nn',
-    version='0.4.2',
+    version='0.5.0',
     description='A Simple, Handwritten Neural Network',
     long_description=long_description,
     license='MIT',
